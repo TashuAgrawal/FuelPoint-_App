4636b0e2cf260c68e7825b359e7c1e2f9a4c3e7f
TashuAgrawal <tashuagrawal67@gmail.com>
2024-10-17 11:25:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.application
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.application.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnNext = findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
+
+
